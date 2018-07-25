bc7e31284ef7e1c45ea3433d0523bb7341df8cae
YOUR NAME <YOUR@EMAIL.com>
2018-07-25 11:32:55 -0400

    add poke api

---
+++ library.js
@@ -0,0 +1,22 @@
+trainer1 = {
+name: 'michael'
+}
+
+trainer2 = {
+name: 'michael'
+}
+
+
+class Trainer {
+constructor(name) {
+name = name
+}
+}
+
+
+
+function callAPI() {
+  xhttp = function() {
+
+  }
+}
